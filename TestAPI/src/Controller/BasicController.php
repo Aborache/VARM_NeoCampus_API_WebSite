57775b1559c242908c29a0cf81bde1ae643700cd
Aborache <mathieu.poquillon@gmail.com>
2019-02-19 16:20:53 +0100

    Initialisation

---
+++ TestAPI/src/Controller/BasicController.php
@@ -0,0 +1,79 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\File\File;
+
+class BasicController extends Controller
+{
+    public function allCSV()
+    {
+       
+        #$number = random_int(0, 100);
+        
+        #return new Response(
+        #    '<html><body>Lucky number: '.$number.'</body></html>'
+        #    );
+        # ceci est un test
+        $publicResourcesFolderPath = $this->get('kernel')->getRootDir() . '/../public/';
+        $filename = "All-All-2017-09-01_2018-09-01.csv";
+        return new BinaryFileResponse($publicResourcesFolderPath.$filename);
+    }
+    public function parser($typeData){
+        $publicResourcesFolderPath = $this->get('kernel')->getRootDir() . '/../public/';
+        $filename = "All-All-2017-09-01_2018-09-01.csv";
+        $temps = "tempo/temps.csv";
+        if (($f = fopen($publicResourcesFolderPath.$temps, "w+")) !== FALSE){
+            if (($handle = fopen($publicResourcesFolderPath.$filename, "r")) !== FALSE) {
+                $cpt = 0;
+                while ((($data = fgetcsv($handle,1000, ";")) !== FALSE)&&($cpt++ < 1000000)) {
+                    //$num = count($data);
+                    if ($data[0] == $typeData){
+                        fputcsv($f,$data,";");
+                    }
+                    //echo "<p> $num champs à la ligne $row: <br /></p>\n";
+                    //$row++;
+                    //for ($c=0; $c < $num; $c++) {
+                    //    echo $data[$c] . "<br />\n";
+                    //}
+                }
+                fclose($handle);
+            }
+            fclose($f);
+            return new BinaryFileResponse($publicResourcesFolderPath.$temps);;
+        }
+        
+    }
+    
+    
+    
+    public function parserLieu($typeData){
+        $publicResourcesFolderPath = $this->get('kernel')->getRootDir() . '/../public/';
+        $filename = "All-All-2017-09-01_2018-09-01.csv";
+        $temps = "tempo/temps.csv";
+        if (($f = fopen($publicResourcesFolderPath.$temps, "w+")) !== FALSE){
+            if (($handle = fopen($publicResourcesFolderPath.$filename, "r")) !== FALSE) {
+                $cpt = 0;
+                while ((($data = fgetcsv($handle,1000, ";")) !== FALSE)&&($cpt++ < 1000)) {
+                    //$num = count($data);
+                    if ((substr_count ($data[1], $typeData)) > 0){
+                        fputcsv($f,$data,";");
+                    }
+                    //echo "<p> $num champs à la ligne $row: <br /></p>\n";
+                    //$row++;
+                    //for ($c=0; $c < $num; $c++) {
+                    //    echo $data[$c] . "<br />\n";
+                    //}
+                }
+                fclose($handle);
+            }
+            fclose($f);
+            return new BinaryFileResponse($publicResourcesFolderPath.$temps);;
+        }
+        
+    }
+    
+    
+}
