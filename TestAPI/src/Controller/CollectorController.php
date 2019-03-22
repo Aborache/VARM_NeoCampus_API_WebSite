56ae2e2edc46b9db3766cca291d3d6f14a57cd7c
Aborache <mathieu.poquillon@gmail.com>
2019-03-22 09:00:43 +0100

    gestion bat|room|cluster|all

--- TestAPI/src/Controller/CollectorController.php
+++ TestAPI/src/Controller/CollectorController.php
@@ -14,8 +14,23 @@ class CollectorController extends BaseAPIController {
     
     function DonneeBase($date_debut, $date_fin, $type_lieu, $lieu, $typeDonne){
         /*http://localhost:8000/Data/2017-09-12%20::23::41/2017-09-12%20::23::45/Cluster/4/1 */
-        return $this->simpleRequest("SELECT valeur,date FROM mesure WHERE
-             date > '".$date_debut."' AND date < '".$date_fin."' AND typeMesure = ".$typeDonne." AND ilot =". $lieu);
+        switch ($type_lieu) {
+            case "Cluster":               
+                return $this->simpleRequest("SELECT valeur,date FROM mesure WHERE
+                    date > '".$date_debut."' AND date < '".$date_fin."' AND typeMesure = ".$typeDonne." AND ilot =". $lieu);
+                break;
+            case "Building":
+                return $this->simpleRequest("SELECT valeur,date FROM mesure, ilot, salle WHERE
+                    date > '".$date_debut."'
+                    AND date < '".$date_fin."' AND typeMesure = ".$typeDonne." AND mesure.ilot = ilot.id 
+                    AND ilot.salle = salle.id AND salle.batiment = ". $lieu."");
+                break;
+            case "Room":
+                return $this->simpleRequest("SELECT valeur,date FROM mesure, ilot WHERE
+                    date > '".$date_debut."' 
+                    AND date < '".$date_fin."' AND typeMesure = ".$typeDonne." AND mesure.ilot = ilot.id AND ilot.salle = ". $lieu."");
+                break;
+        }
         
     }
     
